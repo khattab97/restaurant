072a1b0c4cdca658ff532ae4b7ae368d1d893368
Milk Steak <a.khatttab97@gmail.com>
2022-09-12 15:13:54 +0200

    Reforamt Code

--- src/home.js
+++ src/home.js
@@ -1,83 +1,83 @@
-function loadHome () {
-    let home = document.createElement('div');
-    home.classList.add('home');
+function loadHome() {
+  let home = document.createElement("div");
+  home.classList.add("home");
 
-    function mainDish () {
-        let dish = document.createElement('div');
-        let title = document.createElement('h1');
-        let img = new Image();
-        let dishName = document.createElement('p');
-        let dish_content = document.createElement('p');
+  function mainDish() {
+    let dish = document.createElement("div");
+    let title = document.createElement("h1");
+    let img = new Image();
+    let dishName = document.createElement("p");
+    let description = document.createElement("p");
 
-        dish.classList.add('dish');
-        title.textContent = 'Main Dish';
-        img.src = '../src/images/mac.jpg';
-        dishName.textContent = "Mac's Famous Mac 'n Cheese"
-        dish_content.textContent = "This is macaroni cheese American-style. Gooey," +
-                                    " filling and served with a bacon salad," +
-                                    " there’s no denying this is proper comfort food.";
-        dish.appendChild(title);
-        dish.appendChild(img);
-        dish.appendChild(dishName);
-        dish.appendChild(dish_content);
+    dish.classList.add("dish");
+    title.textContent = "Main Dish";
+    img.src = "../src/images/mac.jpg";
+    dishName.textContent = "Mac's Famous Mac 'n Cheese";
+    description.textContent =
+      "This is macaroni cheese American-style. Gooey," +
+      " filling and served with a bacon salad," +
+      " there’s no denying this is proper comfort food.";
+    dish.appendChild(title);
+    dish.appendChild(img);
+    dish.appendChild(dishName);
+    dish.appendChild(description);
 
-        return dish;
-    }
+    return dish;
+  }
 
-    function hours() {
-        let days = document.createElement('div');
-        let title = document.createElement('h3')
-        let sunday = document.createElement('p');
-        let monday =document.createElement('p');
-        let tuesday =document.createElement('p');
-        let wednesday =document.createElement('p');
-        let thursday =document.createElement('p');
-        let friday =document.createElement('p');
-        let saturday = document.createElement('p');
+  function hours() {
+    let days = document.createElement("div");
+    let title = document.createElement("h3");
+    let sunday = document.createElement("p");
+    let monday = document.createElement("p");
+    let tuesday = document.createElement("p");
+    let wednesday = document.createElement("p");
+    let thursday = document.createElement("p");
+    let friday = document.createElement("p");
+    let saturday = document.createElement("p");
 
-        days.classList.add('days');
-        title.textContent = 'Hours';
-        sunday.textContent = "Sunday: 8am - 8pm";
-        monday.textContent = "Monday: 6am - 6pm";
-        tuesday.textContent = "Tuesday: 6am - 6pm";
-        wednesday.textContent = "Wednesday: 6am - 6pm";
-        thursday.textContent = "Thursday: 6am - 10pm";
-        friday.textContent = "Friday: 6am - 10pm";
-        saturday.textContent = "Saturday: 8am - 10pm";
+    days.classList.add("days");
+    title.textContent = "Hours";
+    sunday.textContent = "Sunday: 8am - 8pm";
+    monday.textContent = "Monday: 6am - 6pm";
+    tuesday.textContent = "Tuesday: 6am - 6pm";
+    wednesday.textContent = "Wednesday: 6am - 6pm";
+    thursday.textContent = "Thursday: 6am - 10pm";
+    friday.textContent = "Friday: 6am - 10pm";
+    saturday.textContent = "Saturday: 8am - 10pm";
 
-        days.appendChild(title);
-        days.appendChild(sunday);
-        days.appendChild(monday);
-        days.appendChild(tuesday);
-        days.appendChild(wednesday);
-        days.appendChild(thursday);
-        days.appendChild(friday);
-        days.appendChild(saturday);
+    days.appendChild(title);
+    days.appendChild(sunday);
+    days.appendChild(monday);
+    days.appendChild(tuesday);
+    days.appendChild(wednesday);
+    days.appendChild(thursday);
+    days.appendChild(friday);
+    days.appendChild(saturday);
 
-        return days;
+    return days;
+  }
 
-    }
+  function location() {
+    let loc = document.createElement("div");
+    let title = document.createElement("h3");
+    let content = document.createElement("p");
 
-    function location (){
-        let loc = document.createElement('div');
-        let title = document.createElement('h3');
-        let content = document.createElement('p');
+    loc.classList.add("loc");
+    title.textContent = "Location";
+    content.textContent = "123 Forest Drive, Forestville, Maine";
 
-        loc.classList.add('loc');
-        title.textContent = "Location";
-        content.textContent = "123 Forest Drive, Forestville, Maine";
+    loc.appendChild(title);
+    loc.appendChild(content);
 
-        loc.appendChild(title);
-        loc.appendChild(content);
+    return loc;
+  }
 
-        return loc
-    }
+  home.appendChild(mainDish());
+  home.appendChild(hours());
+  home.appendChild(location());
 
-    home.appendChild(mainDish());
-    home.appendChild(hours());
-    home.appendChild(location());
-
-    return home;
+  return home;
 }
 
-export {loadHome};
+export { loadHome };
--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
-import {loadHome} from "./home";
+import { loadHome } from "./home";
 
+const content = document.querySelector(".content");
+let container = document.createElement("div");
 
-const content = document.querySelector('.content');
-let container = document.createElement('div');
+function createHeader() {
+  const header = document.createElement("header");
+  const name = document.createElement("h1");
 
-
-function createHeader () {
-    const header = document.createElement('header');
-    const name = document.createElement('h1');
-
-
-    name.textContent = 'BEEF';
-    header.appendChild(name);
-    header.appendChild(createNav());
-    return header;
+  name.textContent = "BEEF";
+  header.appendChild(name);
+  header.appendChild(createNav());
+  return header;
 }
 
-function createNav (){
-    const navBar = document.createElement('nav');
+function createNav() {
+  const navBar = document.createElement("nav");
 
-    const homeBtn = document.createElement('button');
-    homeBtn.textContent = 'Home';
-    const menuBtn = document.createElement('button');
-    menuBtn.textContent = 'Menu';
-    const contactBtn = document.createElement('button');
-    contactBtn.textContent = 'Contact Us';
+  const homeBtn = document.createElement("button");
+  homeBtn.textContent = "Home";
+  const menuBtn = document.createElement("button");
+  menuBtn.textContent = "Menu";
+  const contactBtn = document.createElement("button");
+  contactBtn.textContent = "Contact Us";
 
-    navBar.appendChild(homeBtn);
-    navBar.appendChild(menuBtn);
-    navBar.appendChild(contactBtn);
-    return navBar;
+  navBar.appendChild(homeBtn);
+  navBar.appendChild(menuBtn);
+  navBar.appendChild(contactBtn);
+  return navBar;
 }
 
-container.classList.add('container');
+container.classList.add("container");
 
 content.appendChild(createHeader());
 content.appendChild(container);
